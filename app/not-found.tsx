d69386c818f10b094e0660c862cc202ae0ac0ba2
Aristide Dongo <aristidedongo4@gmail.com>
2024-09-19 14:34:29 +0000

    final etape

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
 import Link from 'next/link'
+import "@/app/not-found.css"
 
 type Props = {}
 
 export default function notfound({}: Props) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 text-white">
-    <h1 className="text-9xl font-extrabold animate-bounce">404</h1>
-    <h2 className="text-4xl font-semibold mt-4 animate-fade-in-down">
-      Oups ! Vous êtes perdu ?
-    </h2>
-    <p className="text-xl mt-2 animate-fade-in-down delay-500">
-      La page que vous cherchez n'existe pas.
-    </p>
-    <div className="mt-8 animate-fade-in-up delay-700">
-      <Link href="/ui/dashboard" className="px-8 py-4 bg-white text-blue-600 font-bold rounded-full shadow-lg hover:bg-gray-100 transition duration-300">
-        Retour à l'accueil
-      </Link>
+    <div className="">
+    <div className="planet">
+      <div className="inner">
+        <div className="surface"></div>
+      </div>
+      <div className="ring"></div>
+      <div className="moon"></div>
+      <span>4</span>
+      <span>4</span>
+      <div className="stars s1"></div>
+      <div className="stars s2"></div>
+      <div className="stars s3"></div>
+      <div className="stars s4"></div>
     </div>
+    <p className="alt">404</p>
+    <p className="info">Not-Found</p>
   </div>
   )
 }
